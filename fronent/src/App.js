0b28be42a1b5a5575f9e9503aab9f1cb4323a4e7
diegomated <diegocardwr@gmail.com>
2022-05-30 16:40:12 -0500

    fronent update

--- fronent/src/App.js
+++ fronent/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
 import ShowProducts from './productos/showproducts.js';
+import ModifyProducts from './productos/modifyproducts.js';
+import AddProducts from './productos/addproducts';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <ShowProducts></ShowProducts>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ShowProducts/>}/>
+          <Route path='/create' element={<AddProducts/>}/>
+          <Route path='/edit/:id' element={<ModifyProducts/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ fronent/src/productos/modifyproducts.js
@@ -0,0 +1,68 @@
+
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const URI = 'http://localhost:3000/product'; 
+
+
+const ModifyProducts = ()=>{
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    const [idproducto, setidproducto] = useState('');
+    const [nombre, setnombre] = useState('');
+    const [valor, setvalor] = useState('');
+    const [stock, setstock] = useState('');
+
+    useEffect(()=>{
+        getproduct();
+    }, []);
+
+    const getproduct = async ()=>{
+        var res = (await axios.get(`${URI}/${id}`)).data[0];
+        setidproducto(res.idproducto);
+        setnombre(res.nombre);
+        setvalor(res.valor);
+        setstock(res.stock);
+    }
+
+    const update = async (e) =>{
+        e.preventDefault();
+        await axios.put(`${URI}/${id}`, {
+            nombre: nombre,
+            valor: valor,
+            stock: stock
+        });
+        navigate('/');
+    };
+
+    return(
+        <div>
+            <h3>Modificar {idproducto}</h3>
+            <form onSubmit={update}>
+                <input
+                    placeholder='Nombre'
+                    value={nombre}
+                    onChange={(e)=> setnombre(e.target.value)}
+                /><br/>
+                <input
+                    type='number'
+                    placeholder='Valor'
+                    value={valor}
+                    onChange={(e)=> setvalor(e.target.value)}
+                /><br/>
+                <input
+                    type='number'
+                    placeholder='Stock'
+                    value={stock}
+                    onChange={(e)=> setstock(e.target.value)}
+                /><br/>
+                <button type="submit" className='btn btn-primary'>Modificar</button>
+            </form>
+        </div>
+    )
+}
+
+export default ModifyProducts;
+
