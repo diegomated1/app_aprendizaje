117da3c0a2ac91306d52438c1fef8aec3b276e21
diegomated <diegocardwr@gmail.com>
2022-05-29 21:49:34 -0500

    fronent settings

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,10 +2,6 @@ import express from 'express';
 const router = express.Router();
 import {getproduct, editproduct, deleteproduct, addproduct} from '../controllers/productsController.js';
 
-router.get('/test', (req, res)=>{
-    res.json(req.body);
-});
-
 router.get('/', getproduct);
 router.get('/:id', getproduct);
 router.post('/', addproduct);
