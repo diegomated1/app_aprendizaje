2e1b3a10220cd6c46f80c8490eda87e6c859faaf
diegomated <diegocardwr@gmail.com>
2022-05-30 18:05:38 -0500

    (client, seller, bussines, user) router

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,13 +1,58 @@
 import express from 'express';
-const router = express.Router();
+const productrouter = express.Router();
+const clientrouter = express.Router();
+const sellerrouter = express.Router();
+const businessrouter = express.Router();
+const userrouter = express.Router();
+
 import {getproduct, editproduct, deleteproduct, addproduct} from '../controllers/productsController.js';
+import {getclient, editclient, deletecliente, addcliente} from '../controllers/clientController.js';
+import {getseller, editseller, deleteseller, addseller} from '../controllers/sellerController.js';
+import {getbusiness, editbusiness, deletebusiness, addbusiness} from '../controllers/businessController.js';
+import {getuser, edituser, deleteuser, adduser} from '../controllers/userController.js';
 
-router.get('/', getproduct);
-router.get('/:id', getproduct);
-router.post('/', addproduct);
-router.put('/:id', editproduct);
-router.delete('/:id', deleteproduct);
+//#region productrouter
+productrouter.get('/', getproduct);
+productrouter.get('/:id', getproduct);
+productrouter.post('/', addproduct);
+productrouter.put('/:id', editproduct);
+productrouter.delete('/:id', deleteproduct);
+//#endregion
 
+//#region clientrouter
+clientrouter.get('/', getclient);
+clientrouter.get('/:id', getclient);
+clientrouter.post('/', addcliente);
+clientrouter.put('/:id', editclient);
+clientrouter.delete('/:id', deletecliente);
+//#endregion
 
+//#region sellerrouter
+sellerrouter.get('/', getseller);
+sellerrouter.get('/:id', getseller);
+sellerrouter.post('/', addseller);
+sellerrouter.put('/:id', editseller);
+sellerrouter.delete('/:id', deleteseller);
+//#endregion
 
-export default router;
+//#region businessrouter
+businessrouter.get('/', getbusiness);
+businessrouter.get('/:id', getbusiness);
+businessrouter.post('/', addbusiness);
+businessrouter.put('/:id', editbusiness);
+businessrouter.delete('/:id', deletebusiness);
+//#endregion
+
+//#region userrouter
+userrouter.get('/', getuser);
+userrouter.get('/:id', getuser);
+userrouter.post('/', adduser);
+userrouter.put('/:id', edituser);
+userrouter.delete('/:id', deleteuser);
+//#endregion
+
+export const product_router = productrouter;
+export const client_router = clientrouter;
+export const seller_router = sellerrouter;
+export const business_router = businessrouter;
+export const user_router = userrouter;
