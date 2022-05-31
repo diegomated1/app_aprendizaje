842bbbe77c7a81eec179b03f249b101453afbd40
diegomated <diegocardwr@gmail.com>
2022-05-31 11:16:50 -0500

    menu user interface update

--- fronent/src/menu/menu.js
+++ fronent/src/menu/menu.js
@@ -2,23 +2,36 @@
 import axios from 'axios';
 import {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const URI = 'http://localhost:3000/'; 
 
 const Menu = ()=>{
+    var iduser = localStorage.getItem('iduser');
+
+    const navigate = useNavigate();
+
+    const cerrarsesion = ()=>{
+        localStorage.removeItem('iduser');
+        navigate('/login');
+    };
+
     return(
         <div className='container'>
             <div>
-                <Link to={'/products'} className="btn btn-info btn-lg">Products</Link>
+                <Link to={`/${iduser}/products`} className="btn btn-info btn-lg">Products</Link>
             </div>
             <div>
-                <Link to={'/business'} className="btn btn-info btn-lg">Business</Link>
+                <Link to={`/${iduser}/business`} className="btn btn-info btn-lg">Business</Link>
             </div>
             <div>
-                <Link to={'/sellers'} className="btn btn-info btn-lg">Sellers</Link>
+                <Link to={`/${iduser}/sellers`} className="btn btn-info btn-lg">Sellers</Link>
             </div>
             <div>
-                <Link to={'/clients'} className="btn btn-info btn-lg">Clients</Link>
+                <Link to={`/${iduser}/clients`} className="btn btn-info btn-lg">Clients</Link>
+            </div>
+            <div>
+                <button onClick={cerrarsesion} className='btn btn-warning'>Cerrar Sesion</button>
             </div>
         </div>
     )
