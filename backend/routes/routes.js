8c051c75b0f4f69a80428dde3019962795fc3bf2
diegomated <diegocardwr@gmail.com>
2022-05-31 11:13:51 -0500

    router backend update

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -9,41 +9,42 @@ import {getproduct, editproduct, deleteproduct, addproduct} from '../controllers
 import {getclient, editclient, deletecliente, addcliente} from '../controllers/clientController.js';
 import {getseller, editseller, deleteseller, addseller} from '../controllers/sellerController.js';
 import {getbusiness, editbusiness, deletebusiness, addbusiness} from '../controllers/businessController.js';
-import {getuser, edituser, deleteuser, adduser} from '../controllers/userController.js';
+import {getuser, edituser, deleteuser, adduser, login} from '../controllers/userController.js';
 
 //#region productrouter
-productrouter.get('/', getproduct);
-productrouter.get('/:id', getproduct);
+productrouter.get('/:iduser', getproduct);
+productrouter.get('/:iduser/:id', getproduct);
 productrouter.post('/', addproduct);
 productrouter.put('/:id', editproduct);
-productrouter.delete('/:id', deleteproduct);
+productrouter.delete('/:iduser/:id', deleteproduct);
 //#endregion
 
 //#region clientrouter
-clientrouter.get('/', getclient);
-clientrouter.get('/:id', getclient);
+clientrouter.get('/:iduser', getclient);
+clientrouter.get('/:iduser/:id', getclient);
 clientrouter.post('/', addcliente);
 clientrouter.put('/:id', editclient);
-clientrouter.delete('/:id', deletecliente);
+clientrouter.delete('/:iduser/:id', deletecliente);
 //#endregion
 
 //#region sellerrouter
-sellerrouter.get('/', getseller);
-sellerrouter.get('/:id', getseller);
+sellerrouter.get('/:iduser', getseller);
+sellerrouter.get('/:iduser/:id', getseller);
 sellerrouter.post('/', addseller);
 sellerrouter.put('/:id', editseller);
-sellerrouter.delete('/:id', deleteseller);
+sellerrouter.delete('/:iduser/:id', deleteseller);
 //#endregion
 
 //#region businessrouter
-businessrouter.get('/', getbusiness);
-businessrouter.get('/:id', getbusiness);
+businessrouter.get('/:iduser', getbusiness);
+businessrouter.get('/:iduser/:id', getbusiness);
 businessrouter.post('/', addbusiness);
 businessrouter.put('/:id', editbusiness);
-businessrouter.delete('/:id', deletebusiness);
+businessrouter.delete('/:iduser/:id', deletebusiness);
 //#endregion
 
 //#region userrouter
+userrouter.post('/login', login);
 userrouter.get('/', getuser);
 userrouter.get('/:id', getuser);
 userrouter.post('/', adduser);
